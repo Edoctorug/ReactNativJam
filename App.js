d9e7588413edb7dd3644def0e8ad4da2171d7faf
jamil-stark <kunoomaking@gmail.com>
2023-01-25 16:02:25 +0300

    added dropdown feature

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
+
 import {
   StyleSheet,
   TextInput,
@@ -8,8 +8,22 @@ import {
   View,
   Text,
   TouchableOpacity,
-  Button,
+  Platform,
 } from "react-native";
+
+import { Dropdown } from 'react-native-element-dropdown';
+
+
+const data = [
+  { label: 'Patient', value: 'patient' },
+  { label: 'Hospital', value: 'hospital' },
+  { label: 'Doctor', value: 'Doctor' },
+  { label: 'Midwife', value: 'Midwife' },
+  { label: 'Item 5', value: '5' },
+  { label: 'Item 6', value: '6' },
+  { label: 'Item 7', value: '7' },
+  { label: 'Item 8', value: '8' },
+];
 
 export default function App() {
   const [fname, onChangeFname] = React.useState("First Name");
@@ -19,14 +33,20 @@ export default function App() {
   const [password, onChangePassword] = React.useState("Password");
   const [cpassword, onChangeCpassword] = React.useState("Re-enter Password");
 
+  const handleSign = () => alert("Registered!");
+
+  const [value, setValue] = React.useState(null);
+  const [isFocus, setIsFocus] = React.useState(false);
+
   return (
-    <ImageBackground source={require("./assets/edocbg.jpg")} resizeMode="cover">
-      <SafeAreaView style={styles.container}>
-        <View>
+    
+      <SafeAreaView style = {{paddingTop: Platform.OS === "android" ? 20 : 0, backgroundColor: "#0055C1"}}>
+        <ImageBackground source={require("./app/assets/edocbg.jpg")} resizeMode="cover">
+        <View style={styles.container}>
           <Text
             style={{
               fontWeight: "bold",
-              color: "DBE4E4",
+              color: "white",
               textAlign: "center",
               fontSize: 60,
             }}
@@ -36,7 +56,7 @@ export default function App() {
           <Text
             style={{
               fontWeight: "bold",
-              color: "DBE4E4",
+              color: "white",
               textAlign: "center",
               fontSize: 20,
             }}
@@ -50,7 +70,7 @@ export default function App() {
               textAlign: "center",
               fontSize: 20,
             }}
-          >
+            >
             you have to please register!
           </Text>
           <TextInput
@@ -63,8 +83,31 @@ export default function App() {
             onChangeText={onChangeLname}
             value={lname}
           />
+            <View style={styles.containerDropDown}>
+      <Dropdown
+        style={[styles.input, isFocus && { borderColor: 'blue' }]}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        inputSearchStyle={styles.inputSearchStyle}
+        iconStyle={styles.iconStyle}
+        data={data}
+        search
+        maxHeight={300}
+        labelField="label"
+        valueField="value"
+        placeholder={!isFocus ? 'Select type' : '...'}
+        searchPlaceholder="Search..."
+        value={value}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        onChange={item => {
+          setValue(item.value);
+          setIsFocus(false);
+        }}
+      />
+    </View>
           <TextInput
-            style={styles.input}
+            style={[styles.input]}
             onChangeText={onChangeEmail}
             value={email}
           />
@@ -78,36 +121,38 @@ export default function App() {
             style={styles.input}
             onChangeText={onChangePassword}
             value={password}
-            // secureTextEntry={true}
+            secureTextEntry={true}
             textContentType="oneTimeCode"
           />
           <TextInput
             style={styles.input}
             onChangeText={onChangeCpassword}
             value={cpassword}
+            secureTextEntry={true}
+            textContentType="oneTimeCode"
           />
           <TouchableOpacity>
-          <Text style={styles.sign}>
-            SignUp
-          </Text>
-        </TouchableOpacity>
+            <Text style={styles.sign} onPress={handleSign}>
+              SignUp
+            </Text>
+          </TouchableOpacity>
         </View>
-        
+        </ImageBackground>
       </SafeAreaView>
-    </ImageBackground>
+    
   );
 }
 
 const styles = StyleSheet.create({
   input: {
-    fontWeight : 'bold',
+    fontWeight: "bold",
     height: 40,
     width: 200,
     margin: 10,
     borderBottomWidth: 2,
     // borderLeftWidth: 1,
     // borderRightWidth: 1,
-    borderColor: 'white',
+    borderColor: "white",
     padding: 11,
     borderBottomLeftRadius: 6,
     borderBottomRightRadius: 6,
@@ -118,10 +163,11 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   container: {
-    padding: 50,
+    padding: 10,
     width: "100%",
     height: "100%",
     alignItems: "center",
+    
   },
   container2: {
     backgroundColor: "#ffff",
@@ -141,7 +187,46 @@ const styles = StyleSheet.create({
     width: 200,
     margin: 10,
     padding: 11,
-    
+
     borderRadius: 30,
+  },
+  containerDropDown: {
+
+    fontSize: "bold",
+  },
+  dropdown: {
+    height: 50,
+    borderColor: 'white',
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  icon: {
+    marginRight: 5,
+  },
+  label: {
+    position: 'absolute',
+    backgroundColor: 'white',
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 8,
+    fontSize: 14,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
   },
 });
---
+++ README.md
@@ -0,0 +1,13 @@
+#EDOCTORUG
+The app is for an online hospital EdoctorUg.
+
+#The following dependencies are installed for it's development.
+├──@babel/core@7.20.12
+├── @react-native-community/hooks@2.8.1
+├── expo-status-bar@1.4.2
+├── expo@47.0.13
+├── react-native-dropdown-picker@5.4.4
+├── react-native-element-dropdown@2.5.3
+├── react-native@0.70.5
+└── react@18.1.0
+
