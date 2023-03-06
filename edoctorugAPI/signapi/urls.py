473fc263ef13bea5acb7755543d6ac8386d47747
Lutaaya Jamil <kunoomaking@gmail.com>
2023-03-06 16:02:10 +0300

    class based view

--- edoctorugAPI/signapi/urls.py
+++ edoctorugAPI/signapi/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 # from .views import TestView
-from .views import account_details, accounts_list
+from .views import ArticleList, ArticleDetails
+# account_details, accounts_list
 # from rest_framework_jwt.views import refresh_jwt_token, verify_jwt_token
 
 urlpatterns = [
-    # path('', TestView.as_view()),
-    path('accounts/', accounts_list),
-    path('accounts/<int:pk>/', account_details),
+    path('accounts/', ArticleList.as_view()),
+    path('accounts/<int:id>/', ArticleDetails.as_view()),
+    # path('accounts/', accounts_list),
+    # path('accounts/<int:pk>/', account_details),
 ]
--- edoctorugAPI/signapi/views.py
+++ edoctorugAPI/signapi/views.py
@@ -1,20 +1,59 @@
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-
-# class TestView(APIView):
-#     def get(self, request, format=None):
-#         print("API has been called")
-#         return Response("It is working", status=200)
-
+from rest_framework.decorators import APIView
 from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
 from .models import UserAccount
 from .serializers import UserAccountSerializer
-from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+
+class ArticleList(APIView):
+    def get(self, request):
+        accounts = UserAccount.objects.order_by('firstname')
+        serializer = UserAccountSerializer(accounts, many=True)
+        return Response(serializer.data)
+    
+
+    def post(self, request):
+        serializer = UserAccountSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class ArticleDetails(APIView):
+    def get_object(self, id):
+        try:
+           return UserAccount.objects.get(id=id)
+    
+        except UserAccount.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+    def get(self, request, id):
+        accounts = self.get_object(id)
+        serializer = UserAccountSerializer(accounts)
+        return Response(serializer.data)        
+    
+
+    def put(self, request, id):
+        account = self.get_object(id)
+        serializer = UserAccountSerializer(account, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+    def delete(self, request, id):
+        account = self.get_object(id)
+        account.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+"""
 
 @api_view(['GET', 'POST'])
 def accounts_list(request):
@@ -52,5 +91,7 @@ def account_details(request, pk):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
      
     elif request.method == "DELETE":
-        article.delete()
+        account.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+"""
